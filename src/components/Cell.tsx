eb03e5f27594a0b0ffb6a7bf6a66d61d8c460a70
Huseyn <elsefter.dayi@gmail.com>
2025-09-30 14:43:43 +0400

    fixing

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,20 +1,9 @@
 import type { CellProps } from "../Models/Models";
 
-interface CellPropsWithSize extends CellProps {
-  size: number;
-}
-
-const Cell = ({ row, col, size }: CellPropsWithSize) => {
+const Cell = ({ row, col }: CellProps) => {
   return (
-    <div
-      className="bg-gray-300 border border-gray-400 flex items-center justify-center cursor-pointer select-none"
-<<<<<<< HEAD
-      style={{ width: size, height: size }}
-=======
->>>>>>> 2b20f1d7717b455c9847d489db2ff440e82a39b3
-      data-row={row}
-      data-col={col}
-    />
+    <div className="bg-gray-300 border border-gray-400 flex items-center justify-center cursor-pointer select-none w-full h-full" 
+         data-row={row} data-col={col} />
   );
 };
 
